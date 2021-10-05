68f384906540e74b0b34c26428f787f2b05ca29f
Connor <c.mccurrie@gmail.com>
2021-10-05 16:52:33 -0500

    Initial Commit

--- Models/NorthwindContext.cs
+++ Models/NorthwindContext.cs
@@ -5,10 +5,11 @@ namespace Northwind.Models
 {
     public class NorthwindContext : DbContext
     {
-        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) {}
+        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) { }
 
-        public DbSet<Product> Products {get; set;}
-        public DbSet<Category> Categories {get; set;}
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
 
     }
 }
